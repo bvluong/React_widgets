7ea7401e37dbf2725fe985cf020debaef0326682
bvluong <bryantvinluong@gmail.com>
2017-05-05 16:59:57 -0700

    complete autocomplete widget

--- Widgets/frontend/autocomplete.jsx
+++ Widgets/frontend/autocomplete.jsx
@@ -4,9 +4,8 @@ import ReactDom from 'react-dom';
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.names);
-    this.names = this.props.names;
-    this.state = { inputVal: "" };
+    this.state = { inputVal: "", newNames: this.props.names};
+    this.click = this.click.bind(this);
     this.handler = this.handler.bind(this);
   }
 
@@ -20,7 +19,12 @@ class AutoComplete extends React.Component {
 
   handler(e){
     e.preventDefault();
-    let newNames = this.names.filter( (name) => name.includes(e.target.value));
+    this.setState({ newNames:this.state.newNames.filter( (name) => name.includes(e.target.value)) });
+  }
+
+  click(e){
+    e.preventDefault();
+    this.setState({ newNames:[e.target.textContent]});
   }
 
   render () {
@@ -29,6 +33,9 @@ class AutoComplete extends React.Component {
     <h1>My AutoComplete</h1>
       <ul>
         <input onChange={this.handler}></input>
+        {
+          this.state.newNames.map( (name,idx) => <li onClick={this.click} key={idx}>{name}</li> )
+        }
       </ul>
     </div>
     );
